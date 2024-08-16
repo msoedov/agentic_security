8d6618487fd20fc2f3a6e1c9202036437084cc25
Alexander Myasoedov <msoedov@gmail.com>
2024-08-16 21:31:43 +0300

    fix(middleware):

--- agentic_security/app.py
+++ agentic_security/app.py
@@ -226,7 +226,11 @@ config.dictConfig(
 
 class LogNon200ResponsesMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        response = await call_next(request)
+        try:
+            response = await call_next(request)
+        except Exception as e:
+            logger.exception("Yikes")
+            raise e
         if response.status_code != 200:
             logger.error(
                 f"{request.method} {request.url} - Status code: {response.status_code}"
