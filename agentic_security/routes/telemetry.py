7e458dbfc49495c19db4a2fa063f1fa86639f63e
Alexander Myasoedov <msoedov@gmail.com>
2025-02-24 19:55:12 +0200

    fix(ignore_errors = [KeyboardInterrupt]):

--- agentic_security/routes/telemetry.py
+++ agentic_security/routes/telemetry.py
@@ -16,6 +16,7 @@ def setup(app):
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for tracing.
         traces_sample_rate=1.0,
+        ignore_errors=[KeyboardInterrupt],
         _experiments={
             # Set continuous_profiling_auto_start to True
             # to automatically start the profiler on when
