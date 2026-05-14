46ec775266c631ce231b226441bedc762896973e
Alexander Myasoedov <msoedov@gmail.com>
2026-05-14 18:40:27 +0300

    feat(deprecate ui):

--- ui/vue.config.js
+++
@@ -1,2 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({ transpileDependencies: true, publicPath: '/' ,devServer: { allowedHosts: 'all', client: {webSocketURL: 'auto://0.0.0.0:0/ws'}}, })
