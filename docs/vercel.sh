669169bd11fef0afa6ef7666e4d19da110353e4a
Alexander Myasoedov <msoedov@gmail.com>
2026-05-14 19:19:44 +0300

    feat(vercel):

--- docs/vercel.sh
+++ docs/vercel.sh
@@ -2,8 +2,9 @@ set -ex
 
 python3 --version
 
-pip3 install --upgrade pip
-pip3 install \
+# Vercel's Python is uv-managed (PEP 668 externally-managed), so pip needs
+# --break-system-packages. Safe here: the build container is ephemeral.
+pip3 install --break-system-packages \
   mkdocs \
   mkdocs-material \
   mkdocs-jupyter \
