08fc1d49dd0e1dc1b8ad9c69d9cc9a71420cc4c8
Saturin <pompomsaturin@gmail.com>
2026-08-03 22:52:45 +0100

    export module protocol

---
+++ agentic_security/probe_data/modules/__init__.py
@@ -0,0 +1,8 @@
+"""Dynamic probe module interfaces and implementations."""
+
+from agentic_security.probe_data.modules.module_protocol import (
+    ModuleOutput,
+    ModuleProtocol,
+)
+
+__all__ = ["ModuleOutput", "ModuleProtocol"]
